03bfe97f93cdbd57c4035a3367554ea374f9a52b
tedweb123 <103286695+tedweb123@users.noreply.github.com>
2022-06-13 22:30:02 +0200

    Add files via upload

---
+++ js/comunes.js
@@ -0,0 +1,25 @@
+    
+/*musica de fondo*/
+var music = new Audio('./sonidos/genkidama.mp3');
+music.play(); 
+document.addEventListener("keydown", function() {
+	keyReturn(event);
+});
+/*FIN musica de fondo*/
+        
+  window.onload = () => {
+            setTimeout(() => {
+                
+            }, 3000);
+        }
+
+        function refrescarPagina() {
+            location.reload();
+        }
+  
+    
+
+	
+	 
+    
+
